8b43252bc5e495744f92e9e90c9b5e8467c7cb0c
McSymilian <77546701+McSymilian@users.noreply.github.com>
2023-04-10 21:20:13 +0200

    Revert "Added extensions to locomotives and train cars"

--- src/main/java/com/rydwas/trainworld/train/locmotive/Locomotive.java
+++ src/main/java/com/rydwas/trainworld/train/locmotive/Locomotive.java
@@ -1,13 +1,11 @@
 package com.rydwas.trainworld.train.locmotive;
 
 import lombok.Getter;
-import org.jetbrains.annotations.NotNull;
 
 import static com.rydwas.trainworld.train.locmotive.LocomotiveIdGenerator.generateId;
 
 public abstract class Locomotive {
     @Getter
-    @NotNull
     private final Integer id = generateId();
 
 }
